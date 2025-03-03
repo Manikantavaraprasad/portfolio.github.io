466fdf5f889d4b1fef2b0149658fdbad12644fee
Manikantavaraprasad <140079240+Manikantavaraprasad@users.noreply.github.com>
2025-03-03 12:55:19 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,3 +22,27 @@ $('.navbar-collapse ul li a').click(function() {
 
 
 
+$(document).ready(function() {
+    $('#contact-form').on('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+
+        // Serialize form data
+        var formData = $(this).serialize();
+
+        // Send the AJAX request
+        $.ajax({
+            url: 'mailer.php', // The PHP file to handle the form submission
+            type: 'POST',
+            data: formData,
+            beforeSend: function() {
+                $('#form-message').html('<p>Sending your message...</p>');
+            },
+            success: function(response) {
+                $('#form-message').html(response); // Display success or error message
+            },
+            error: function() {
+                $('#form-message').html('<p>Something went wrong, please try again later.</p>');
+            }
+        });
+    });
+});
